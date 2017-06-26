b9988075335eaa2f2b8e8150aa7e35a4bfe9b917
David Wolgemuth <davidjwolgemuth@gmail.com>
2017-06-26 10:31:27 -0500

    initial commit

---
+++ repos.js
@@ -0,0 +1,422 @@
+const REPOS = [
+  {
+    "id": 94808189,
+    "fullName": "cd-chicago-june-cohort/fake-blog-david",
+    "name": "fake-blog-david",
+    "createdAt": "2017-06-19T18:27:33Z",
+    "updatedAt": "2017-06-19T18:29:08Z",
+    "pushedAt": "2017-06-19T18:33:24Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94810544,
+    "fullName": "cd-chicago-june-cohort/fake-blog-Alyssa",
+    "name": "fake-blog-Alyssa",
+    "createdAt": "2017-06-19T18:56:25Z",
+    "updatedAt": "2017-06-19T18:58:33Z",
+    "pushedAt": "2017-06-19T18:58:32Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94811613,
+    "fullName": "cd-chicago-june-cohort/day1-John",
+    "name": "day1-John",
+    "createdAt": "2017-06-19T19:09:56Z",
+    "updatedAt": "2017-06-19T19:12:27Z",
+    "pushedAt": "2017-06-19T20:26:30Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94813520,
+    "fullName": "cd-chicago-june-cohort/Dan-s-projects",
+    "name": "Dan-s-projects",
+    "createdAt": "2017-06-19T19:33:42Z",
+    "updatedAt": "2017-06-20T19:09:46Z",
+    "pushedAt": "2017-06-20T19:09:45Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94813630,
+    "fullName": "cd-chicago-june-cohort/forms.html",
+    "name": "forms.html",
+    "createdAt": "2017-06-19T19:35:05Z",
+    "updatedAt": "2017-06-19T19:35:05Z",
+    "pushedAt": "2017-06-19T19:35:05Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94816202,
+    "fullName": "cd-chicago-june-cohort/fake-blog-baldmike",
+    "name": "fake-blog-baldmike",
+    "createdAt": "2017-06-19T20:04:24Z",
+    "updatedAt": "2017-06-19T21:28:23Z",
+    "pushedAt": "2017-06-19T21:28:21Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94817164,
+    "fullName": "cd-chicago-june-cohort/forms",
+    "name": "forms",
+    "createdAt": "2017-06-19T20:16:45Z",
+    "updatedAt": "2017-06-19T20:16:45Z",
+    "pushedAt": "2017-06-19T20:16:46Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94820193,
+    "fullName": "cd-chicago-june-cohort/GitHubRepoAssignment_Alyssa",
+    "name": "GitHubRepoAssignment_Alyssa",
+    "createdAt": "2017-06-19T20:56:14Z",
+    "updatedAt": "2017-06-19T20:56:14Z",
+    "pushedAt": "2017-06-19T21:03:40Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94824653,
+    "fullName": "cd-chicago-june-cohort/GitHubRepoAssigment",
+    "name": "GitHubRepoAssigment",
+    "createdAt": "2017-06-19T22:01:26Z",
+    "updatedAt": "2017-06-19T22:01:26Z",
+    "pushedAt": "2017-06-19T22:08:31Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94827793,
+    "fullName": "cd-chicago-june-cohort/GitHubRepoAssignment",
+    "name": "GitHubRepoAssignment",
+    "createdAt": "2017-06-19T22:57:24Z",
+    "updatedAt": "2017-06-19T22:57:24Z",
+    "pushedAt": "2017-06-19T23:02:00Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94829708,
+    "fullName": "cd-chicago-june-cohort/GitHubRepoAssignment-John",
+    "name": "GitHubRepoAssignment-John",
+    "createdAt": "2017-06-19T23:35:01Z",
+    "updatedAt": "2017-06-19T23:35:01Z",
+    "pushedAt": "2017-06-19T23:39:32Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94830429,
+    "fullName": "cd-chicago-june-cohort/fake-blog-erick",
+    "name": "fake-blog-erick",
+    "createdAt": "2017-06-19T23:50:21Z",
+    "updatedAt": "2017-06-19T23:52:08Z",
+    "pushedAt": "2017-06-19T23:52:06Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94832589,
+    "fullName": "cd-chicago-june-cohort/Scott-fake-blog.",
+    "name": "Scott-fake-blog.",
+    "createdAt": "2017-06-20T00:30:55Z",
+    "updatedAt": "2017-06-20T00:31:41Z",
+    "pushedAt": "2017-06-20T00:31:40Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94833895,
+    "fullName": "cd-chicago-june-cohort/ScottGitHubRepoAssignment",
+    "name": "ScottGitHubRepoAssignment",
+    "createdAt": "2017-06-20T00:53:51Z",
+    "updatedAt": "2017-06-20T00:53:51Z",
+    "pushedAt": "2017-06-20T00:54:25Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94834682,
+    "fullName": "cd-chicago-june-cohort/GitHubRepoAssignment-Erick",
+    "name": "GitHubRepoAssignment-Erick",
+    "createdAt": "2017-06-20T01:06:01Z",
+    "updatedAt": "2017-06-20T01:06:01Z",
+    "pushedAt": "2017-06-20T01:12:52Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94853352,
+    "fullName": "cd-chicago-june-cohort/forum",
+    "name": "forum",
+    "createdAt": "2017-06-20T05:33:40Z",
+    "updatedAt": "2017-06-20T05:33:40Z",
+    "pushedAt": "2017-06-20T05:33:41Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94911064,
+    "fullName": "cd-chicago-june-cohort/box-spacing-demo-david",
+    "name": "box-spacing-demo-david",
+    "createdAt": "2017-06-20T16:12:58Z",
+    "updatedAt": "2017-06-20T16:13:18Z",
+    "pushedAt": "2017-06-20T16:13:17Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94921506,
+    "fullName": "cd-chicago-june-cohort/box_spacing_Sam_and_Alyssa",
+    "name": "box_spacing_Sam_and_Alyssa",
+    "createdAt": "2017-06-20T18:12:51Z",
+    "updatedAt": "2017-06-20T18:13:07Z",
+    "pushedAt": "2017-06-20T18:13:04Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94923292,
+    "fullName": "cd-chicago-june-cohort/erick-john-boxes-project",
+    "name": "erick-john-boxes-project",
+    "createdAt": "2017-06-20T18:33:49Z",
+    "updatedAt": "2017-06-20T18:35:44Z",
+    "pushedAt": "2017-06-20T18:35:42Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94925939,
+    "fullName": "cd-chicago-june-cohort/Plotting_Your_Blocks_Alyssa",
+    "name": "Plotting_Your_Blocks_Alyssa",
+    "createdAt": "2017-06-20T19:07:30Z",
+    "updatedAt": "2017-06-20T19:07:54Z",
+    "pushedAt": "2017-06-20T19:07:53Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94928025,
+    "fullName": "cd-chicago-june-cohort/Julian-Scott-Dan_Blockassignment",
+    "name": "Julian-Scott-Dan_Blockassignment",
+    "createdAt": "2017-06-20T19:33:15Z",
+    "updatedAt": "2017-06-20T19:33:45Z",
+    "pushedAt": "2017-06-20T19:33:44Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94931853,
+    "fullName": "cd-chicago-june-cohort/plotting-blocks-Sam",
+    "name": "plotting-blocks-Sam",
+    "createdAt": "2017-06-20T20:23:14Z",
+    "updatedAt": "2017-06-20T20:23:35Z",
+    "pushedAt": "2017-06-20T20:23:34Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94931918,
+    "fullName": "cd-chicago-june-cohort/JavaScript_Basics_Alyssa",
+    "name": "JavaScript_Basics_Alyssa",
+    "createdAt": "2017-06-20T20:23:55Z",
+    "updatedAt": "2017-06-20T20:24:07Z",
+    "pushedAt": "2017-06-20T20:24:05Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94932158,
+    "fullName": "cd-chicago-june-cohort/john-plotting-blocks",
+    "name": "john-plotting-blocks",
+    "createdAt": "2017-06-20T20:26:43Z",
+    "updatedAt": "2017-06-20T20:32:37Z",
+    "pushedAt": "2017-06-20T20:32:36Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94932455,
+    "fullName": "cd-chicago-june-cohort/john-plotting-blocks-for-real",
+    "name": "john-plotting-blocks-for-real",
+    "createdAt": "2017-06-20T20:30:59Z",
+    "updatedAt": "2017-06-20T20:30:59Z",
+    "pushedAt": "2017-06-20T20:30:59Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94932514,
+    "fullName": "cd-chicago-june-cohort/CSS-Boxes-BM",
+    "name": "CSS-Boxes-BM",
+    "createdAt": "2017-06-20T20:31:52Z",
+    "updatedAt": "2017-06-20T22:52:12Z",
+    "pushedAt": "2017-06-20T20:32:44Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94934978,
+    "fullName": "cd-chicago-june-cohort/-john-jsbasics",
+    "name": "-john-jsbasics",
+    "createdAt": "2017-06-20T21:05:20Z",
+    "updatedAt": "2017-06-20T22:22:07Z",
+    "pushedAt": "2017-06-20T22:22:05Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94936401,
+    "fullName": "cd-chicago-june-cohort/erick-plotting-box",
+    "name": "erick-plotting-box",
+    "createdAt": "2017-06-20T21:25:39Z",
+    "updatedAt": "2017-06-20T21:25:58Z",
+    "pushedAt": "2017-06-20T21:25:57Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94940564,
+    "fullName": "cd-chicago-june-cohort/plotbox-baldmike",
+    "name": "plotbox-baldmike",
+    "createdAt": "2017-06-20T22:33:08Z",
+    "updatedAt": "2017-06-20T22:33:08Z",
+    "pushedAt": "2017-06-20T22:33:08Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  },
+  {
+    "id": 94942039,
+    "fullName": "cd-chicago-june-cohort/Scott_Block_Assignment",
+    "name": "Scott_Block_Assignment",
+    "createdAt": "2017-06-20T23:00:02Z",
+    "updatedAt": "2017-06-21T13:55:42Z",
+    "pushedAt": "2017-06-21T13:55:41Z",
+    "owner": {
+      "id": 29548641,
+      "login": "cd-chicago-june-cohort",
+      "admin": false,
+      "type": "Organization"
+    }
+  }
+];
